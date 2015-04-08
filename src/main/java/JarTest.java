135c40b8caae73739b7d7569d06bf3444362e1a3
Giuliano Lancioni <lancioni@uniroma3.it>
2015-04-09 00:51:28 +0200

    corrected wrong reference

--- src/main/java/JarTest.java
+++ src/main/java/JarTest.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.jplurk.PlurkSettings;
-import com.google.jplurk.exception.PlurkException;
-
 /**
  * Servlet implementation class JarTest
  */
