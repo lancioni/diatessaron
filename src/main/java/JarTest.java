c4cac2c0dd21d9d5833590cd46c08440c1548bcf
Giuliano Lancioni <lancioni@uniroma3.it>
2015-04-09 11:43:27 +0200

    added compiled library jgrapht-jdk1.6.jar

--- src/main/java/JarTest.java
+++ src/main/java/JarTest.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.net.*;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+
 /**
  * Servlet implementation class JarTest
  */
@@ -34,8 +39,44 @@ public class JarTest extends HttpServlet {
 	      // Actual logic goes here.
 	      PrintWriter out = response.getWriter();
 	      out.println("<h1 align=\"right\">" + message + "</h1>");
+	      
+	        UndirectedGraph<String, DefaultEdge> stringGraph = createStringGraph();
+
+	        // note undirected edges are printed as: {<v1>,<v2>}
+	        out.println(stringGraph.toString());
 	  }
 	  
+	  
+	  /**
+	     * Craete a toy graph based on String objects.
+	     *
+	     * @return a graph based on String objects.
+	     */
+	    private static UndirectedGraph<String, DefaultEdge> createStringGraph()
+	    {
+	        UndirectedGraph<String, DefaultEdge> g =
+	            new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+
+	        String v1 = "v1";
+	        String v2 = "v2";
+	        String v3 = "v3";
+	        String v4 = "v4";
+
+	        // add the vertices
+	        g.addVertex(v1);
+	        g.addVertex(v2);
+	        g.addVertex(v3);
+	        g.addVertex(v4);
+
+	        // add edges to create a circuit
+	        g.addEdge(v1, v2);
+	        g.addEdge(v2, v3);
+	        g.addEdge(v3, v4);
+	        g.addEdge(v4, v1);
+
+	        return g;
+	    }
+	    
 	  public void destroy()
 	  {
 	      // do nothing.
