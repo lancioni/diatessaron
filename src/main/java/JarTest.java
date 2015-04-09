50f95264c9fbc9e545d13c3a1061baba90239506
Giuliano Lancioni <lancioni@uniroma3.it>
2015-04-09 02:10:23 +0200

    void call to openccg added

--- src/main/java/JarTest.java
+++ src/main/java/JarTest.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import opennlp.ccg.grammar.*;
+
 /**
  * Servlet implementation class JarTest
  */
@@ -15,6 +17,7 @@ public class JarTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	private String message;
+	private Grammar grammar;
 
 	  public void init() throws ServletException
 	  {
