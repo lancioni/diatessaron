cf925f3100838439c4d380370a63b72f81e655fc
Giuliano Lancioni <lancioni@uniroma3.it>
2015-04-12 22:45:08 +0200

    dummy login servlet added

---
+++ src/main/java/com/rhcloud/semdic/diatessaron/ADPLogin.java
@@ -0,0 +1,56 @@
+package com.rhcloud.semdic.diatessaron;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ADPLogin
+ */
+@WebServlet(description = "class that manages login to the ArabicDiatessaronProject", urlPatterns = { "/ADPLogin" })
+public class ADPLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String message;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ADPLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+	      message = "تسجيل الدخول";
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  // Set response content type
+		  response.setCharacterEncoding("UTF-8");
+	      response.setContentType("text/html");
+
+	      // Actual logic goes here.
+	      PrintWriter out = response.getWriter();
+	      out.println("<h1 align=\"right\">" + message + "</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
