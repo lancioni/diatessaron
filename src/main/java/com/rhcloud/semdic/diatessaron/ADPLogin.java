871a19506382a603a55b6eee2d64694dc92ce171
Giuliano Lancioni <lancioni@uniroma3.it>
2015-04-25 22:00:36 +0200

    First retry after merging problems

--- src/main/java/com/rhcloud/semdic/diatessaron/ADPLogin.java
+++ src/main/java/com/rhcloud/semdic/diatessaron/ADPLogin.java
@@ -138,14 +138,6 @@ public class ADPLogin extends HttpServlet {
 			response.sendRedirect(encodedURL);
 			MongoCollection<Document> diacoll = mongoDB.getCollection("diatessaron");
 
-			/*DBObject qryV1 = (DBObject) JSON.parse("{'_id':'1','verses._id':'1'}");
-			DBObject projection = (DBObject) JSON.parse("{'verses.$':'1'}");
-			FindIterable<Document> foundQryV1 = diacoll.find((Bson) qryV1).projection((Bson) projection);
-			Document firstQryV1 = foundQryV1.first();
-			if (firstQryV1 != null) {
-				ArrayList verses = (ArrayList) firstQryV1.get("verses");
-				
-			}*/
 			ctx.setAttribute("diacoll", mongoDB.getCollection("diatessaron"));
 		}else{
 	    	request.setAttribute("error", "Unknown user or password, please try again."); // Set error.
